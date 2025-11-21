a459741336f72743ceab397d847988618c7bdbaa
Hendrik Postma <hendrik.postma@han.nl>
2025-11-21 21:46:10 +0100

    feat: Implement CMD Datapunten Helper with HAN styling and AI integration

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'han-red': '#E50056',
+        'han-black': '#141414',
+        'han-gray': '#F1F1F1',
+      },
+      fontFamily: {
+        sans: ['"Avenir Next"', 'sans-serif'],
+        condensed: ['"Avenir Next Condensed"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 } 
